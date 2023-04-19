c0f48a0019103b7b76082a01abe94af77fd1814a
laivwxyz <delacruzlailanie2004@gmail.com>
2023-04-19 13:55:32 +0800

    Pseudocode has been added in doing the Vigenere Cipher

--- the_vigenere_cipher.py
+++ the_vigenere_cipher.py
@@ -1,3 +1,8 @@
 # Dela cruz, Lailanie E. _ BSCPE 1-4
 # Assignment 2
 # Problem 3 - The Vigenere Cipher
+
+# Function generates key in a continuous process until it's length isn't equal to the length of original text
+# With the help of the key, the function returns the encrypted text generated
+# Use the decryption function to decrypt the encrypted text and returns the original text
+# FUnction asks the user for the plaintext and print out the output of the program 
