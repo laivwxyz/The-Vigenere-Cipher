e05b3761fcfeabf0d9738ba86b9a33b6ac466528
laivwxyz <delacruzlailanie2004@gmail.com>
2023-04-19 15:21:13 +0800

    key length checking has been added

--- the_vigenere_cipher.py
+++ the_vigenere_cipher.py
@@ -3,6 +3,11 @@
 # Problem 3 - The Vigenere Cipher
 
 # Function generates key in a continuous process until it's length isn't equal to the length of original text
+def generate_key(message, key):
+    key = list(key)
+    if len(message) == len(key):
+        return(key)
+
 # With the help of the key, the function returns the encrypted text generated
 # Use the decryption function to decrypt the encrypted text and returns the original text
 # FUnction asks the user for the plaintext and print out the output of the program 
